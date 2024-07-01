01796a0878a9bdc8f255970e1ce6bb9ea1b76ab5
Svygzhryr <pamixy@gmail.com>
2024-07-01 14:55:15 +0300

    feat: add necessary packages

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Library } from './pages/Library'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <Library />
+    </React.StrictMode>
+)
