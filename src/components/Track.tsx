ebcd2da415ed46f0eadfc267436b5eea4a723cb9
Svygzhryr <pamixy@gmail.com>
2024-07-12 08:41:36 +0300

    feat: add duration bar and dynamic play button

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -1,16 +1,37 @@
-import { FC, useEffect, useRef } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import styles from '../styles/modules/track.module.scss'
 import { TrackProps } from '../types/types'
 
 export const Track: FC<TrackProps> = ({ name, cover, file }) => {
     const audioRef = useRef<HTMLAudioElement | null>(null)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [currentDuration, setCurrentDuration] = useState(0)
+
+    function startPlayback(track: HTMLAudioElement) {
+        setIsPlaying(true)
+        track.play()
+    }
+
+    function stopPlayback(track: HTMLAudioElement, interval: NodeJS.Timeout) {
+        clearInterval(interval)
+        setIsPlaying(false)
+        track.pause()
+    }
 
     function handleClick() {
         const track = audioRef.current as HTMLAudioElement
         if (!track) return
 
-        track.paused ? track.play() : track.pause()
+        const interval = setInterval(() => {
+            setCurrentDuration(audioRef.current?.currentTime || 0)
+        }, 1000)
+
+        track.paused ? startPlayback(track) : stopPlayback(track, interval)
     }
+
+    useEffect(() => {
+        console.log(currentDuration)
+    }, [currentDuration])
 
     return (
         <div className={styles.wrapper} onClick={handleClick}>
@@ -21,8 +42,21 @@ export const Track: FC<TrackProps> = ({ name, cover, file }) => {
                 <div className={styles.mask}></div>
                 <img src={cover} alt="" className={styles.cover} />
                 <div className={styles.controls}>
-                    <button className={styles.pause}></button>
-                    <div className={styles.progressBar}></div>
+                    <button
+                        className={` ${isPlaying ? styles.play : styles.pause}`}
+                    ></button>
+
+                    <div
+                        style={{
+                            width: `${
+                                (currentDuration / audioRef.current?.duration) *
+                                100
+                            }%`,
+                        }}
+                        className={styles.progressBar}
+                    >
+                        {audioRef.current?.duration}
+                    </div>
                 </div>
             </div>
         </div>
